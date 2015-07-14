583282848fa7d0a9a672d993d0e25636c9f053b4
Robert McWilliam <rmcw@allmail.net>
2015-07-14 22:53:46 +0100

    Change links in events bar to be relative.

--- eventsbar.php
+++ eventsbar.php
@@ -1,4 +1,4 @@
 <p>
-<a href="/regularevents" class="btn btn-default">Regular Events</a>
-<a href="/calendar" class="btn btn-default">Calendar</a>
+<a href="regularevents" class="btn btn-default">Regular Events</a>
+<a href="calendar" class="btn btn-default">Calendar</a>
 </p>
