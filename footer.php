8e3aa2facc3288058901cbea8af3bd4118089b66
Robert McWilliam <rmcw@allmail.net>
2015-07-14 22:47:45 +0100

    Update copyright statement in footer to include up to now.

--- footer.php
+++ footer.php
@@ -34,7 +34,7 @@
         </aside> <!-- footer-widgets -->
 
         <footer id="footer">
-            <p style="max-width: 800px; margin: 0 auto;">&copy; 2014 <a href="mailto:contact@57north.co">57North Hacklab Ltd.</a>. All rights reserved. 57North Hacklab Ltd. is a company registered in Scotland (No. SC470230). 57North and 57North Hacklab are trading names of 57North Hacklab Ltd.</p>
+            <p style="max-width: 800px; margin: 0 auto;">&copy; 2013-<?php echo date("Y"); ?> <a href="mailto:contact@57north.co">57North Hacklab Ltd.</a>. All rights reserved. 57North Hacklab Ltd. is a company registered in Scotland (No. SC470230). 57North and 57North Hacklab are trading names of 57North Hacklab Ltd.</p>
 <p><a href="/hotpink" style="color: #FF69B4; margin-right: 10px;" class="pull-right"><b class="fa fa-square"></b></a></p>
 
         </footer>
