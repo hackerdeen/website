f1483afcac4fc891911c1fcdb6caa78dec947db3
Robert McWilliam <rmcw@allmail.net>
2017-10-10 21:52:50 +0100

    Add heading for calendar

--- calendar.php
+++ calendar.php
@@ -8,7 +8,8 @@
         <p>Every Tuesday from around 1900h there is a meetup at the space not only for members but also for those that may want to join us. Typically, the projects people will be working on will be announced on the mailing list in advance of the meeting. Come along to work on your project, socialise with members, or if you're new, just to find out more about us.</p>
     
         <p>Of course, the space is always available for members to access via <a href="https://wiki.57north.org.uk/index.php/Projects:Doorbot">doorbot</a>. If you do not have a key to the front door, it is always a good idea to co-ordinate with one of the keyholders beforehand in case the building has been locked.</p>
-          
+
+        <h1>Calendar</h1>
         <div id="calendar"></div>
         <p><i>Powered by <a href="http://opentechcalendar.co.uk/">Open Tech Calendar</a>.</i></p>
       </div>
