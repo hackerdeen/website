71af947e29f3da6f5d4df31c59115ccca2737da2
Andy Gaskell <ag@ssofb.co.uk>
2018-09-18 22:48:12 +0100

    new donate page

---
+++ donate.php
@@ -0,0 +1,34 @@
+<?php define('IN_SITE', 1); $page = "About"; 
+
+$description = "Learn more about 57North Hacklab, our board of directors, how to communicate with us via IRC and e-mail and how to become a member of our hackerspace.";
+
+/*
+This pages requires stripe
+https://github.com/stripe/stripe-php
+
+
+*/
+
+include('header.php'); ?>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Donate to 57North</h1>
+            </div>
+        </div><!-- row -->
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Lorem Ipsum</h2>
+                <p>Vivamus mollis, purus lacinia malesuada facilisis, metus nisi laoreet ante, egestas iaculis metus erat at nisi. Suspendisse vel placerat mauris, laoreet luctus tortor. Suspendisse potenti. Sed purus nisi, feugiat a feugiat quis, lobortis a felis. Proin laoreet tortor at fringilla pretium. Sed lobortis metus lorem, vel eleifend nisl maximus sed. Duis malesuada ultrices tincidunt. Donec vel pharetra est, porttitor rhoncus orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            </div>
+            <div class="col-md-6">
+                <h1>What is a hackspace?</h1>
+                <p>Button.</p>
+            </div>
+        </div><!-- row -->        
+    </div><!-- container -->
+</section>		
+
+<?php include('footer.php'); ?>
+
