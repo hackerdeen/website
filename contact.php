e2e2be1a03bcafe05c3a98a7aa1284a21b679d17
Iain Learmonth <irl@fsfe.org>
2018-10-26 09:16:54 +0100

    contact: Information fixes and expansion

--- contact.php
+++ contact.php
@@ -8,8 +8,9 @@
 
 <table class="table">
 <tr><th valign="top">Email</th><td><a href="mailto:contact@57north.co">contact@57north.co</a></td></tr>
-<tr><th valign="top">Phone</th><td><a href="tel:+441224583491">+441224583491</a></td></tr>
-<tr><th valign="top">Physical Address</th><td>57North Hacklab<br>Downstiars from Engage Gaming.<br>26 North Silver Street.<br>Aberdeen.<br/>AB10 1RL.<br>Scotland</td></tr>
+<tr><th valign="top">Phone</th><td><a href="tel:+49221596191057">+49221596191057</a> (watch out, this is a German phone number for now!)</td></tr>
+<tr><th valign="top">SIP</th><td><a href="sip:1057@hg.eventphone.de">sip:1057@hg.eventphone.de</a></td></tr>
+<tr><th valign="top">Postal Address</th><td>57North Hacklab<br>Downstiars from Engage Gaming.<br>26 North Silver Street.<br>Aberdeen.<br/>AB10 1RL.<br>Scotland</td></tr>
 <tr><th valign="top">Mailing List</th><td><p>For important announcements, you can subscribe to our announcements mailing list, and/or to engage in hacklab discussions you can subscribe to the discussion mailing list via the <a href="http://lists.57north.co/">web interface</a>.</p>
     <p> For more details, see our <a href="https://wiki.57north.org.uk/index.php/Mailing_List">mailing list pages on the wiki</a>.</p>
 </td></tr>
@@ -25,6 +26,10 @@ location lat/lon
 
 <iframe width="1140" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-2.116949558258057%2C57.14371392357426%2C-2.0959210395812993%2C57.1511288226186&amp;layer=mapnik&amp;marker=57.1474215588468%2C-2.1064352989196777" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/?mlat=57.14742&amp;mlon=-2.10644#map=17/57.14742/-2.10644&amp;layers=N">View Larger Map</a></small>
 
+<h3>Access</h3>
+
+Our entrance can be found on Skene Terrace via the fire escape between the Assembly Rooms and the steps up to North Silver Street. If you are not a member of the space but expect that someone is in, then either a doorbell will be attached to the left of the door frame or you can call the phone number listed above.
+    
 <h3>Transport Info</h3>
 
 <h4>Busses</h4>
@@ -35,7 +40,6 @@ location lat/lon
 
 <h4>Driving / Parking</h4>
 <p>Because of the city center location parking can sometimes be sub-optimal, but there is metered on-street parking on silver street and golden square, and often plenty of spaces in the Denburn multi-story car park.</p>
-
 
 </div></div><!-- row -->
 </section>
