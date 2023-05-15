10100e628221a162727944a97da0af8cb8997a81
Andy Gaskell <ag@ssofb.co.uk>
2023-05-15 16:50:57 +0100

    Added more
    - Hospitalfield
    - Aberdeen Amateur Radio Society
    
     Add a "Resources" page #40

--- resources.php
+++ resources.php
@@ -22,21 +22,19 @@ include('header.php'); ?>
                     <li><a href="https://www.dundeemakerspace.com/" target="_blank">Dundee Makerspace</a></li>                    
                 </ul>
 
-                <h2>Hacklabs related Orgs</h2>
+                <h2>Hacklab and Hackspace related Orgs</h2>
                 <ul>
                     <li><a href="https://www.hackspace.org.uk/" target="_blank">Hackspace Foundation</a></li>
                     <li><a href="https://vulca.eu/tour" target="_blank">Vulka Tour</a></li>
                     <li><a href="https://www.ccc.de/en" target="_blank">Chaos Computer Club</a></li>
                 </ul>
 
-                <h2>Hacklabs type Events</h2>
+                <h2>Hacklab and Hackspace type Events</h2>
                 <ul>
                     <li><a href="https://www.emfcamp.org/" target="_blank">EMF Camp</a></li>
                     <li><a href="https://events.ccc.de/" target="_blank">CCC, Chaos Communication Camp</a></li>
                     <li><a href="https://wiki.milliways.info" target="_blank">Milliways</a></li>      
                 </ul>
-
-
 
                 <h2>Member projects and side things</h2>
                 <ul>
@@ -48,16 +46,17 @@ include('header.php'); ?>
             </div>
             <div class="col-md-6">
 
-                <h2>Tech Orgs and Spaces in Aberdeen</h2>
+                <h2>Tech Orgs and Spaces in and around Aberdeen</h2>
                 <ul>
                     <li><a href="https://pythonaberdeen.github.io/" target="_blank">Aberdeen Python User Group</a></li>
                     <li><a href="https://codethecity.org/" target="_blank">Code The City</a></li>
                     <li><a href="https://aberlug.org.uk/" target="_blank">Aberdeen Linux User Group</a></li>
                     <li><a href="https://www.thisiscodebase.com/aberdeen" target="_blank">ONE CodeBase Aberdeen</a></li>
                     <li><a href="https://aberdeensciencecentre.org/" target="_blank">Aberdeen Science Centre</a></li>
+                    <li><a href="http://www.aars.org.uk/" target="_blank">Aberdeen Amateur Radio Society</a></li>
                 </ul>                
 
-                <h2>Creative Orgs and Spaces in Aberdeen</h2>
+                <h2>Creative Orgs and Spaces in and around Aberdeen</h2>
                 <ul>
                     <li><a href="https://www.aberdeencity.gov.uk/AAGM" target="_blank"> Aberdeen Archives, Gallery and Museums</a></li>
                     <li><a href="https://peacock.studio/" target="_blank">Peacock Studio</a></li>
@@ -73,6 +72,8 @@ include('header.php'); ?>
                     <li><a href="https://throwupgallery.com" target="_blank">throwupgallery</a></li>
                     <li><a href="https://lookagainaberdeen.co.uk/" target="_blank">Look Again</a></li>
                     <li><a href="https://www.ghat-art.org.uk/" target="_blank">Grampian Hospitals Art Trust (GHAT)</a></li>
+                    <li><a href="https://hospitalfield.org.uk/" target="_blank">Hospitalfield</a></li>
+                    
                 </ul>
 
                 <h2>Creative News in Aberdeen</h2>
