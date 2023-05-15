0c46d70d457bd24e64f9d63f2440eba043732d4e
Andy Gaskell <ag@ssofb.co.uk>
2023-05-15 13:56:11 +0100

    membership page, starter for 10
    Add a "Membership" page #41

---
+++ membership.php
@@ -0,0 +1,63 @@
+<?php define('IN_SITE', 1); $page = "Membership"; 
+
+$description = "Find out about being a Member and what Membership entails.";
+
+include('header.php'); ?>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Membership</h1>
+            </div>
+        </div><!-- row -->
+        <div class="row">
+            <div class="col-md-6">
+
+                <h2>Members</h2>
+
+                <p>Of course there would not be a hacklab were it not for our members. Our membership spans many technical fields with those that have knowledge and experience and also those that wish to learn. The hacklab exists for everyone that would like to use it. We welcome everyone, and enthusiastically celebrate diversity.</p>
+
+                <p><a href="https://wiki.57north.org.uk/doku.php?id=admin:statement_on_equality_and_diversity" title="Statement on Equality and Diversity">Anyone</a> is welcome to join 57North hacklab. We recommend that you visit us a few times on Open Tuesday first to get a feel for the community and meet some friendly faces!</p>
+
+                <p>Once you know you want to join, to fill out our membership form link tba. The board will acknowledge your application and confirm whether it has been accepted within a week.</p>
+
+                <p>Membership costs £20 per month.</p>
+
+                <h2>Remote Members</h2>
+
+                <p>Remote members of the hackerspace are those who live >80km from the centre of Aberdeen. This is intended for our supporters and friends who come to an event or two a year and want to help us out as a community.</p>
+
+                <p>Membership costs a minimum of £2.50 a month.</p>
+
+                <p>Remote members have the same benefits as above, but we reserve the right to not issue with a key, or only temporarily grant you a key to allow access when you are within 80km of Aberdeen.</p>
+
+            </div>
+            <div class="col-md-6">
+
+                <h2>Membership Benefits</h2>
+
+                <h3>Access</h3>
+
+                <p>As a member, you get a key for 24/7 access to the hackerspace. This allows you to not only work on projects, but invite guests to the hackerspace, run events (weekly meetups, weekend hackathons/lan parties/etc) and use the equipment and tools of the space as and when you see fit.</p>
+
+                <p>It is expected that when running an event regardless of whether it is weekend or regular and open to the public or not, you email the -discuss mailing list first to seek community agreement and to ensure that others aren't planning on doing something at the same time!</p>
+
+                <h3>Storage</h3>
+
+                <p>As a member of the hackerspace, you will be allowed to store projects, equipment and other hardware at the hackerspace. Typically members have had a single cardboard box, however as we have more space members have expanded and taken up more room!</p>
+
+                <p>We expect that items are stored in boxes on our storage shelves, and that permanent change of a free workspace must be discussed with the wider community.</p>
+
+                <h3>Guests</h3>
+
+                <p>Members of the hackerspace are entitled to invite non members into our space to use our tools, workspace or just be part of the wider community. We ask that you take responsibility for tidying any and all mess and guest items are cleared as if they are your own.</p>
+
+            </div>
+        </div><!-- row -->
+    
+        
+    </div><!-- container -->
+</section>		
+
+<?php include('footer.php'); ?>
+
