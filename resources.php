c4a8bd81ab9252a6d7895897e6069239a171acdd
Andy Gaskell <ag@ssofb.co.uk>
2023-05-15 13:29:11 +0100

    very initial Resources page
    Add a "Resources" page #40

---
+++ resources.php
@@ -0,0 +1,85 @@
+<?php define('IN_SITE', 1); $page = "Resources"; 
+
+$description = "Some useful resources that are in our general sphere of interest.";
+
+include('header.php'); ?>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Resources</h1>
+                <p style="max-width: none">There are so many great resources online, we thought it'd be useful to try keep a handy list of them here.</p>
+            </div>
+        </div><!-- row -->
+        
+        <div class="row">
+            <div class="col-md-6">
+           
+                <h2>Other Hacklabs</h2>
+                <ul>
+                    <li><a href="https://edinburghhacklab.com/" target="_blank">Edinburgh Hacklab</a></li>
+                    <li><a href="https://www.tog.ie/" target="_blank">Tog Hackerspace, The Dublin Hackerspace</a></li>
+                </ul>
+
+                <h2>Hacklabs related Orgs</h2>
+                <ul>
+                    <li><a href="https://www.hackspace.org.uk/" target="_blank">Hackspace Foundation</a></li>
+                    <li><a href="https://vulca.eu/tour" target="_blank">Vulka Tour</a></li>
+                </ul>
+
+                <h2>Hacklabs type Events</h2>
+                <ul>
+                    <li><a href="https://www.emfcamp.org/" target="_blank">EMF Camp</a></li>
+                    <li><a href="https://events.ccc.de/" target="_blank">CCC, Chaos Communication Camp</a></li>
+                    <li><a href="https://wiki.milliways.info" target="_blank">Milliways</a></li>
+                </ul>
+
+                <h2>Member projects and side things</h2>
+                <ul>
+                    <li><a href="https://www.scottishconsulate.org/" target="_blank">Scottish Consulate</a></li>
+                    <li>CampGND</li>
+                    <li><a href="https://streetartaberdeen.org/" target="_blank">Street Art Aberdeen</a></li>
+                </ul>                
+
+            </div>
+            <div class="col-md-6">
+
+                <h2>Tech Orgs and Spaces in Aberdeen</h2>
+                <ul>
+                    <li><a href="https://pythonaberdeen.github.io/" target="_blank">Aberdeen Python User Group</a></li>
+                    <li><a href="https://codethecity.org/" target="_blank">Code The City</a></li>
+                    <li><a href="https://aberlug.org.uk/" target="_blank">Aberdeen Linux User Group</a></li>
+                    <li><a href="https://www.thisiscodebase.com/aberdeen" target="_blank">ONE CodeBase Aberdeen</a></li>
+                </ul>                
+
+                <h2>Creative Orgs and Spaces in Aberdeen</h2>
+                <ul>
+                    <li><a href="https://www.aberdeencity.gov.uk/AAGM" target="_blank"> Aberdeen Archives, Gallery and Museums</a></li>
+                    <li><a href="https://peacock.studio/" target="_blank">Peacock Studio</a></li>
+                    <li><a href="https://worm.gallery/" target="_blank">The Worm Gallery</a></li>
+                    <li>The Anatomy Rooms</li>
+                    <li><a href="https://www.aberdeencity.gov.uk/services/leisure-culture-and-parks/creative-learning" target="_blank">Aberdeen Council Creative Learning</a></li>
+                    <li><a href="https://outerspaces.org" target="_blank">Outer Spaces</a></li>
+                    <li><a href="https://www.creativeaberdeen.co.uk/" target="_blank">Creative Aberdeen</a></li>
+                    <li><a href="https://www.leopardarts.co.uk/" target="_blank">Leopard Arts</a></li>
+                    <li><a href="https://elev8arts.co.uk/" target="_blank">elev8arts</a></li>
+                    <li><a href="https://www.northernartsclub.co.uk" target="_blank">Northern Arts Club</a></li>
+                    <li><a href="https://www.deemouthartiststudios.co.uk" target="_blank">Deemouth Artist Studios</a></li>
+                    <li><a href="https://throwupgallery.com" target="_blank">throwupgallery</a></li>
+                    <li><a href="https://lookagainaberdeen.co.uk/" target="_blank">Look Again</a></li>
+                </ul>
+
+                <h2>Creative News in Aberdeen</h2>
+                <ul>
+                    <li><a href="https://postabdn.com/" target="_blank">Aberdeen Post</a></li>
+                </ul>
+
+                
+            </div>
+        </div><!-- row -->     
+        
+    </div><!-- container -->
+</section>		
+
+<?php include('footer.php'); ?>
+
