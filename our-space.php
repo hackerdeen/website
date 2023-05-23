6d7b4a7b7539709ce5d180d2822a462c160fc26d
Andy Gaskell <ag@ssofb.co.uk>
2023-05-23 17:37:51 +0100

    New "our space" page, with floorplan
    Add an "Our Space" page #38

---
+++ our-space.php
@@ -0,0 +1,98 @@
+<?php define('IN_SITE', 1); $page = "Our Space"; 
+
+$description = "Our space is a great mix of workshop, studio and social spaces.";
+
+include('header.php'); ?>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Our Space</h1>
+            </div>
+        </div><!-- row -->
+        <div class="row">
+            <div class="col-md-6">
+                <p>Our current space is 57North Hacklab's third location, and the largest so far.  We have around TBC m2 which is separated into several zones for different activities.  The interior is a fairly normal modern open-plan office set-up with durable carpet and a suspended ceiling.</p>
+                <p>There is plenty of space to host sit down meetings of perhaps up to 20 people, and the space probably has a comfortable capacity of around 30 or 40 people.</p>
+                <p>Worth noting, the layout map below is probably out of date already.</p>
+            </div>
+            <div class="col-md-6">
+                <p><img src="/img/outside_all_w550.jpg" alt="The outside of the space"></p>
+            </div>
+        </div><!-- row -->
+
+        <div class="row">
+            <div class="col-md-12">
+                <h2>The layout</h2>
+                <p><img src="/img/our_space_layout.svg" alt="A layout diagram of our space"></p>
+            </div>
+        </div><!-- row -->
+
+
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Work / social spaces</h2>
+                <p>We've a couple of large tables, in two separate spaces, that are used for light hacking, meetings and socialising.</p>
+
+                <h2>Tools</h2>
+                <p>We've a great set of hand tools and power tools.</p>
+
+                <h2>Components</h2>
+                <p>There ar lots of varying sized draws for electrical components and other consumables here.</p>
+                
+                <h2>Fasteners</h2>
+                <p>All the nuts, bolds, screws, fasteners and fixings you'll ever need.</p> 
+                
+                <h2>Cutting & finishing</h2>
+                <p>This section of the space is for more dirty making work, with Dremels, a pillar drill and bandsaw here.</p>
+
+                <h2>Material store</h2>
+                <p>There is a variety of materials here for members to use, wood, plastic, metal, in various forms.</p>
+                
+                <h2>Laser cutter</h2>
+                <p>Our kindly donated laser cutter lives in the corner.</p>
+
+                <h2>Soldering & electronics stations</h2>
+                <p>There are two well equipped desks set up with various tools, including soldering irons, oscilloscope, re-work station, crimpers and power supplies.</p>
+
+                <h2>Video desk</h2>
+                <p>This is set-up to film technical and instructional videos.</p>
+
+                <h2>Radio desk</h2>
+                <p>A few of our members are keen armature radio people, so we have an excellent radio set-up.</p>
+            </div>
+            <div class="col-md-6">
+                <h2>Drinks & snacks</h2>
+                <p>We've a food telly (aka microwave), kettle, fridge and snack bar thing set-up here.</p> 
+                
+                <h2>Museum</h2>
+                <p>If it's too old to use, but too cool to throw away or recycle, it belongs in the museum.</p>
+                
+                <h2>Large printer</h2>
+                <p>We've a large format printer and scanner.</p>
+
+                <h2>3d printer</h2>
+                <p>The 3d printer lives here, with all the printer material in the cupboard beside it.</p>
+
+                <h2>Vinyl cutter</h2>
+                <p>The vinyl cutter is usaful for signs and stencils..</p>
+
+                <h2>Music & lights</h2>
+                <p>We've a pair of CDJs and a mixer, with a lighting rig.</p>
+                
+                <h2>Kitchenette</h2>
+                <p>Not very exciting, but we have a small sink kitchenette in an alcove.</p> 
+                
+                <h2>Sofas & chill space</h2>
+                <p>Plenty of comfy chairs and sofas, where folk can chill and chat.  Also with a TV and a couple of old consoles.</p>
+
+                <h2>Back door</h2>
+                <p>Takes you down stairs to the large car park at the back of the building.</p>
+            </div>
+        </div><!-- row -->    
+        
+    </div><!-- container -->
+</section>		
+
+<?php include('footer.php'); ?>
+
